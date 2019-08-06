87eec8d913e8b01d96a141b74fa421d6af1aa897
Divya <divya.j.nagaraj@gmail.com>
2019-08-05 21:23:43 -0700

    new change

--- strings/string-func.py
+++ strings/string-func.py
@@ -1,9 +1,17 @@
 def reverse_str(string1):
+    temp = string1.split(' ')
+    print(type(temp))
     str1 = ""
-    for ch in string1:
-        str1 = ch + str1
-        print(ch, str1)
-    print(str1)
+
+    list2 = []
+    for i in range(0, len(temp)):
+        str1 = temp[i]
+        str2 = ""
+        for ch in str1:
+            str2 = ch + str2
+        list2.append(str2)
+
+    print(' '.join(list2))
 
 def substring_p(str1, sub1):
     if str.find(str1,sub1) == -1:
@@ -15,7 +23,8 @@ def substring_p(str1, sub1):
     else:
         print("is a substring")
 
+
 if __name__ == '__main__':
-    reverse_str("hello")
-    substring_p("hello", "lo")
-    substring_p("hello", "ho")
+    reverse_str("hello i am divya")
+    # substring_p("hello", "lo")
+    # substring_p("hello", "ho")
